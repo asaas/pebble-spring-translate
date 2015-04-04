2fc1381623064dff2ac93e05a7370f7e3b3cc617
UshakovVasilii <UshakovVasilii@gmail.com>
2015-04-04 15:38:32 +0300

    remove pebble-spring3-dependency, update pebble version

--- src/test/java/com/asaas/pebble/spring/translate/conf/TemplateLoaderTestConfig.java
+++ src/test/java/com/asaas/pebble/spring/translate/conf/TemplateLoaderTestConfig.java
@@ -2,14 +2,14 @@ package com.asaas.pebble.spring.translate.conf;
 
 import org.springframework.context.annotation.Bean;
 
+import com.mitchellbosecke.pebble.loader.ClasspathLoader;
 import com.mitchellbosecke.pebble.loader.Loader;
-import com.mitchellbosecke.pebble.spring.PebbleTemplateLoader;
 
 public class TemplateLoaderTestConfig {
 
     @Bean
     public Loader templateLoader() {
-        PebbleTemplateLoader templateLoader = new PebbleTemplateLoader();
+        ClasspathLoader templateLoader = new ClasspathLoader();
         templateLoader.setPrefix("templates/");
         templateLoader.setSuffix(".peb");
         return templateLoader;
